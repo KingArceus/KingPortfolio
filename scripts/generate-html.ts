a68bab666d8b8c513f8cf3b39199627646f162dd
KingArceus <khangtpc@gmail.com>
2025-05-10 16:51:58 +0700

    Add subfolder

--- scripts/generate-html.ts
+++ scripts/generate-html.ts
@@ -7,7 +7,7 @@ const generateHTML = () => {
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <link rel="icon" type="image/svg+xml" href="/KingPortfolio/favicon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="${siteConfig.description}" />
     <meta name="author" content="${siteConfig.author}" />
@@ -15,10 +15,12 @@ const generateHTML = () => {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <script type="module" crossorigin src="/KingPortfolio/assets/index-fth2OuIn.js"></script>
+    <link rel="stylesheet" crossorigin href="/KingPortfolio/assets/index-Z8hi_5yV.css">
   </head>
   <body>
     <div id="root"></div>
-    <script type="module" src="/src/main.tsx"></script>
+    <script type="module" src="/KingPortfolio/src/main.tsx"></script>
   </body>
 </html>`;
 
